b822e466147e0b9ddc313348a65a02f65891fc91
Asahi Kuroki <kurok011@umn.edu>
2025-07-01 13:24:00 +0900

    init

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+/*
+ "View" means behaves like a View (Protocol): requires "body" computed property
+ When defining a custom view, it requires a "body"; however, things like
+ VStack, Image, Text are builtin Views, thus dont require a body
+ "some" is a opaque type -> body type conforms to View, but the exact type
+ depends on the body's content
+ ex) Text is a struct that conforms to View
+
+ */
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("😁").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12) 
+            }
+           
+        }
     }
 }
 
